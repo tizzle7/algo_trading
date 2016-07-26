8f5ec2b60d1f2962e2ee33a5ab8da8762f942640
titianteiger <titians@gmx.ch>
2016-07-26 23:46:12 +0200

    backtest.py containing abstract base classes for simple object-oriented backtester added.

---
+++ backtester1/backtest.py
@@ -0,0 +1,39 @@
+# backtest.py
+
+from abc import ABCMeta, abstractmethod
+
+class Strategy(metaclass=ABCMeta):
+    """Abstract base class providing an interface for Strategy objects.
+
+    A strategy objetc receives a Pandas DataFrame of prices at a particular
+    frequency and returns a DataFrame of signals, consisting of a timestamp and a
+    long (1), hold (0) or short (-1) signal respectively.
+    """
+    @abstractmethod
+    def generate_signals(self):
+        """A method to generate the signals DataFrame is required for each
+        Strategy instance.
+        """
+        raise NotImplementedError("generate_signals() method needs to be" \
+                                  "implemented!")
+
+class Portfolio(metaclass=ABCMeta):
+    """Interface for the Portfolio class which performs the major backtesting
+    work.
+
+    The portfolio object receives a signals DataFrame and creates a series of
+    of positions and cash components. Also produces an equity curve and keeps
+    track of the trades.
+    """
+    @abstractmethod
+    def generate_positions(self):
+        """A method to generate the positions DataFrame needs to be defined."""
+        raise NotImplementedError("genereate_positions() method needs to be" \
+                                  "implemented!")
+
+    @abstractmethod
+    def backtest_portfolio(self):
+        """
+        """
+        raise NotImplementedError("backtest_portfolio() method needs to be" \
+                                  "implemented!")
